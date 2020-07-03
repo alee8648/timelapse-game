247162f29c880ef0d8b4622a6a74b06c26fda356
Adam Lee <alee8648@gmail.com>
2020-07-03 12:27:52 +0100

    Updates

---
+++ precache-manifest.8b07679705ae2d3ff12a12addf2e5464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2237dbe66930c02bf2ec2d9868cc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab3816fb908fcce3da2f",
+    "url": "/static/css/main.e0432beb.chunk.css"
+  },
+  {
+    "revision": "b557b671b1057ad75244",
+    "url": "/static/js/2.fcb02124.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/static/js/2.fcb02124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab3816fb908fcce3da2f",
+    "url": "/static/js/main.36a49d0d.chunk.js"
+  },
+  {
+    "revision": "30d8f603d4e8837ff90e",
+    "url": "/static/js/runtime-main.0b833b45.js"
+  }
+]);
